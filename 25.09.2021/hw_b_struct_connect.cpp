1ac2cc4f44e1b856ad87b06dbe778a85c9e7f526
denizaurus <denisenko.aa@phystech.edu>
2021-10-05 17:44:28 +0300

    Hw 4 // Final

--- 25.09.2021/hw_b_struct_connect.cpp
+++ 25.09.2021/hw_b_struct_connect.cpp
@@ -28,71 +28,41 @@ struct Student
 	std::string name;
 };
 
-typedef std::variant <Student *, Lesson *> school_struct;
-
 void enroll(std::vector < std::string > & input, // wanted to do with std::visit but couldn't figure out how
-	std::vector < school_struct > linked,
-	const school_struct self)
+	std::vector < Lesson > & linked, Student & self)
 {
-	
-	if (std::holds_alternative < Student *>(self))
+	for (Lesson & lesson : linked)
 	{
-		Student * holding = std::get < 0 >(self);
+		auto temp = std::find(std::begin(input), std::end(input), lesson.course);
 
-		for (school_struct to_link : linked)
+		if (temp != std::end(input))
 		{
-			Lesson * held = std::get < 1 >(to_link);	
-
-			auto temp = std::find(std::begin(input), std::end(input), held->course);
-
-			if (temp != std::end(input))
-			{
-				holding->v.push_back(held);
-				held->v.push_back(holding);
-				std::cout << "Enrolling student " << holding->name << " at course " << held->course << std::endl;
-			}
+			self.v.push_back(&lesson);
+			lesson.v.push_back(&self);
+			std::cout << "Enrolling student " << lesson.v.back()->name << " at course " << lesson.course << std::endl;
 		}
 	}
-	else
-	{
-		Lesson * holding = std::get < 1 >(self);
-
-		for (school_struct to_link : linked)
-		{
-			Student * held = std::get < 0 >(to_link);
-
-			auto temp = std::find(std::begin(input), std::end(input), held->name);
-
-			if (temp != std::end(input))
-			{
-				holding->v.push_back(held);
-				held->v.push_back(holding);
-				std::cout << "Enrolling student " << held->name << " at course " << holding->course << std::endl;
-			}
-		}
-	}
-	
-
 }
 
 int main()
 {
-	std::time_t t = std::chrono::system_clock::to_time_t(
-		std::chrono::system_clock::now());
-
-	const std::vector < std::string > s = { "English", "Geography", "Quantum Chemistry", "Art" };
+	auto t = std::chrono::system_clock::now().time_since_epoch().count();
 	const std::vector < std::string > e = { "Jane Doe", "Mary Sue", "Gary Stu", "John Smith" };
+	std::vector < std::string > s = { "English", "Geography", "Quantum Chemistry", "Art" };
 	std::vector < std::string > desired(4, "");
 
-	std::vector < school_struct > students(4);
-	std::vector < school_struct > lessons(4);
+	std::vector < Student > students = {};
+	std::vector < Lesson > lessons = {};
+
+
 
 	for (auto i = 0U; i < 4; ++i)
 	{
-		// i do not know what to put here so it compiles
+		students.push_back(Student(e[i]));
+		lessons.push_back(Lesson(s[i]));
 	}
 
-	for (school_struct f : students)
+	for (auto & f : students)
 	{
 		std::shuffle(std::begin(s), std::end(s), std::default_random_engine(t));
 		desired = { s[0], s[1] };
@@ -102,14 +72,13 @@ int main()
 
 	// SYSTEM OUTPUT
 
-	for (school_struct l : lessons)
+	for (auto & l : lessons)
 	{
-		Lesson * k = std::get < 1 >(l);
-		std::cout << "-----------------------" << k->course << "-----------------------" << std::endl;
-		for (school_struct f : k->v)
+		std::cout << "-----------------------" << l.course << "-----------------------" << std::endl;
+		for (auto & f : l.v)
 		{
-			Student * g = std::get < 0 >(f);
-			std::cout << k->course << ' ' << g->name << ' ' << &(g->name) << std::endl; // attempts to monitor memory location
+			std::cout << l.course << std::endl;
+			std::cout << ' ' << f->name << ' ' << std::endl; // attempts to monitor memory location
 		}
 	}
 
