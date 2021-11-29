d57d6493bda2ae821d71deb3984d6330b1789eb5
denizaurus <denisenko.aa@phystech.edu>
2021-11-30 00:00:32 +0300

    HW12 Correction

--- 2021.11.20/C.cpp
+++ 2021.11.20/C.cpp
@@ -1,22 +1,19 @@
 #include < iostream >
 #include < cmath >
 
-constexpr size_t factorial(size_t i)
-{
-	if (i == 0) return 1;
-	else return factorial(i - 1) * i;
-}
-
 constexpr double eps_approx(double eps)
 {
 	double value = 1.0;
 	double epsilon = 1.0 / eps;
 
-	for (auto i = 1U;
-		factorial(i) < epsilon;
-		++i)
+	for (auto i = 1U; true; ++i)
 	{
-		value += 1.0 / factorial(i);
+		double factorial = 1.0;
+		for (auto j = i; j > 1.0; --j) { factorial *= j; }
+
+		if (factorial > epsilon) break;
+
+		value += 1.0 / factorial;
 	}
 
 	return value;
@@ -25,8 +22,11 @@ constexpr double eps_approx(double eps)
 
 int main()
 {
-	std::cout << eps_approx(0.001) << std::endl;
-	std::cout << eps_approx(0.0000001) << std::endl;
+	constexpr auto e1 = eps_approx(0.001);
+	constexpr auto e2 = eps_approx(0.0000001);
+
+	std::cout << e1 << std::endl;
+	std::cout << e2 << std::endl;
 
 	return 0;
 }
