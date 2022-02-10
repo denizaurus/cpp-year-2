a00d097d638a0b623aee74988cf1a821b5b089c8
denizaurus <denisenko.aa@phystech.edu>
2022-02-10 03:07:23 +0300

    branch commit

--- 2022.02.05/git_task.cpp
+++ 2022.02.05/git_task.cpp
@@ -14,6 +14,11 @@ int main(int argc, char ** argv)
 	return EXIT_SUCCESS;
 }
 
+int branch_function()
+{
+	//this function exists within the branch
+	return 0;
+}
 //and this is a comment. astounding
 
 // this comment was added within GitHub. wow, technology has advanced a lot
