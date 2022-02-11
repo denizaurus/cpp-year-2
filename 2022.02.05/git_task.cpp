51707735b4b5fecba8c9a47ade067f6176db007e
denizaurus <denisenko.aa@phystech.edu>
2022-02-12 01:41:18 +0300

    hw13 resetting main for next part

--- 2022.02.05/git_task.cpp
+++ 2022.02.05/git_task.cpp
@@ -6,15 +6,3 @@ int main(int argc, char ** argv)
 	system("pause");
 	return EXIT_SUCCESS;
 }
-
-void main_for_merging()
-{
-	//function in main for merging TASK B
-	return;
-}
-
-void branch_for_merging()
-{
-	//funtion in branch_commits for merging task B
-	return;
-}
