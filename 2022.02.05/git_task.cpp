b531549a9b5e60c6c65739a455a5f35e221c653d
denizaurus <denisenko.aa@phystech.edu>
2022-02-12 00:59:13 +0300

    HW13 CORRECTION timer + git_task reset

--- 2022.02.05/git_task.cpp
+++ 2022.02.05/git_task.cpp
@@ -1,41 +1,8 @@
 #include < iostream >
 #include < string >
 
-std::string wow_a_function()
-{
-	return "Wow, I can't believe this is a function!";
-}
-
 int main(int argc, char ** argv)
 {
-	
-
 	system("pause");
 	return EXIT_SUCCESS;
-}
-
-int branch_function()
-{
-	//this function exists within the branch
-	return 0;
-}
-//and this is a comment. astounding
-
-// this comment was added within GitHub. wow, technology has advanced a lot
-
-int main_branch()
-{
-	// this comment exists within the main branch
-	return 0;
-}
-int main_branch_2()
-{
-	return 0;
-}
-
-int rebasing_branch()
-{
-	// now to use REBASE. this exists within the branch
-
-	return 0;
 }
--- 2022.02.05/timer.cpp
+++ 2022.02.05/timer.cpp
@@ -1,54 +1,78 @@
 #include <chrono>
+#include <string>
 #include <iostream>
 
+template < typename T = std::chrono::microseconds >
 class Timer
 {
 public:
 	using clock_t = std::chrono::steady_clock;
 	using time_point_t = clock_t::time_point;
-	using duration_t = clock_t::duration;
+	using duration_t = std::chrono::nanoseconds;
 
-	Timer() : m_start(m_start.time_since_epoch()),
-		m_stored(m_stored.zero()), timer_active(false) {}
 
-	void start() 
+	explicit Timer(int mode, std::string & name) : m_start(clock_t::now()),
+		m_stored(duration_t::zero()), m_name(name), timer_active(false) 
+	{ start(mode); }
+
+	explicit Timer(std::string & name) : m_start(clock_t::now()),
+		m_stored(duration_t::zero()), m_name(name), timer_active(false) {}
+
+	explicit Timer(int mode) : m_start(clock_t::now()),
+		m_stored(duration_t::zero()), m_name("Unnamed"), timer_active(false) 
+	{ start(mode); }
+
+	explicit Timer() : m_start(clock_t::now()),
+		m_stored(duration_t::zero()), m_name("Unnamed"), timer_active(false) {}
+
+	~Timer() noexcept { end(true); }
+
+
+	void start(int mode = 1) 
 
 	{ 
-		m_start = clock_t::now(); 
-		timer_active = true; 
+		if (mode)
+		{
+			if (!timer_active) { m_start = clock_t::now(); }
+			timer_active = true;
+		}
 	}
 
 	void pause() 
 	{ 
-		m_stored += clock_t::now() - m_start; 
+		if (timer_active) { m_stored += clock_t::now() - m_start; }
 		timer_active = false;
 	}
 
-	void end()
+	void output()
 	{
-		if (timer_active)
-		{
-			pause();
-		}
-
-		std::cout << std::chrono::duration_cast <
-			std::chrono::microseconds > (m_stored).count() << " microseconds." << std::endl;
-
+		std::cout << m_name + ": " << std::chrono::duration_cast <T> (m_stored).count() << " units." << std::endl;
 		m_stored = m_stored.zero();
+	}
+
+	void end(bool destr = false)
+	{
+		if (timer_active) { pause(); output();}
+		else if (!timer_active && destr) { pause(); }
+		else { output(); }
 	}
 
 private:
 	time_point_t m_start;
 	duration_t m_stored;
+	std::string m_name;
 	bool timer_active;
 };
 
 // TESTING
 int main()
 {
+	std::string one = "Microseconds";
+	std::string two = "Nanoseconds";
 
-	Timer t;
-	Timer g;
+
+	Timer < std::chrono::microseconds > t(one);
+	Timer < std::chrono::nanoseconds > g(two);
 
 	t.start();
 	g.start();
